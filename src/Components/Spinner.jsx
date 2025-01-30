3069c53bb3c7a33eacff399480bc9e520210ed41
13nour11 <nourhanfahmy010@gmail.com>
2025-01-30 17:23:27 +0200

    change loading color

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { BeatLoader } from 'react-spinners';
+import React from "react";
+import { BeatLoader } from "react-spinners";
 
 export default function Spinner() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <BeatLoader color="#36d7b7" />
+      <BeatLoader color="#0ea5e9" />
     </div>
   );
 }
